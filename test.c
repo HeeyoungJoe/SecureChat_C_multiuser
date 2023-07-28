41d45114c1f90666591bb9f4e66b6af6838be0a9
HeeyoungJoe <heeyoungjoe2000@gmail.com>
2023-07-28 15:35:13 +0900

    test

--- test.c
+++ test.c
@@ -27,7 +27,7 @@ int main(){
     Stub *newstub;
     strncpy(newstub->name,"Joe",3);
     strncpy(newstub->usercode,"001",3);
-
+    printf("Stub:%s\n",newstub->usercode);
     char code_init_message[9];
     strncpy(code_init_message,"10",2);
     strncpy(code_init_message+2,newstub->usercode,3);
