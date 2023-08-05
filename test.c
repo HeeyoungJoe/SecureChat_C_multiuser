e576f5b6c71eece68da455828f354f948af7b8ed
HeeyoungJoe <heeyoungjoe2000@gmail.com>
2023-08-05 13:30:31 +0900

    client.h

--- test.c
+++ test.c
@@ -14,8 +14,17 @@ int main(){
     char user_code[6];
     strncpy(user_code,"000111",6);
     char public_key[247]={"0"};
+
+    // Test node creation
     // Root user node 
     root = newNode(sockfd, nickname,user_code,public_key);
     now = root;
+
+    printf("\n[TEST.C/NEW NODE] Root: %s has %s as code and %s as public_key\n",root->user_name,root->user_code,root->public_key);
+
+    // Test node update 
+    updateUserName(root,user_code,"Keith");
+    printf("\n[TEST.C/UPDATE USERNAME] Root: %s has %s as code and %s as public_key\n",root->user_name,root->user_code,root->public_key);
+
     return 0;
 }
