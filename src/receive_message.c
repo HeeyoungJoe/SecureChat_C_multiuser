659d17af510078a35d7c842a8f7da7a4c5479eee
HeeyoungJoe <heeyoungjoe2000@gmail.com>
2023-09-22 03:15:48 -0700

    needs to check whether message is always last

--- src/receive_message.c
+++ src/receive_message.c
@@ -1,4 +1,9 @@
 
+# define KEY_SIZE 256
+
+int	natoi(char *str, int n);
+int	strs1equal(char *s1, char *s2);
+
 // 1. Receiving
 // finding the type of message
 // cutting down string into several string of information 
@@ -10,5 +15,31 @@ int	return_message_type(char *str)
 {
 	if (str == NULL)
 		return -1;
+	int message_type = natoi(str, 2);
+	return message_type;
+}
 
-	int message_type = putnbr
+char	**return_splitted_message(char *str, char **commands)
+{
+	int	idx = 0;
+	char	**splitted = (char *)malloc(sizeof(char *) * 10);
+	int	splitted_idx = 0;
+	while (commands[idx] != '\0')
+	{
+		if (strnequal("usercode", commands[idx]) == 1)
+		{
+			splitted[splitted_idx++] = strndup(str, 6);
+			str += 6;
+		}
+		else if (strnequal("key", commands[idx]) == 1)
+		{
+			splitted[splitted_idx++] = strndup(str, KEY_SIZE);
+			str += KEY_SIZE;
+		}
+		else if (strendswith("message", commands[idx]) == 1)
+		{
+			splitted[splitted_idx++] = strndup(str
+		idx++;
+
+			
+
